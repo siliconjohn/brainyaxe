040ac7ed944e1adb1a0af51773da0297e44e77d1
John Doerfler <johndoerfler@gmail.com>
2016-07-04 13:29:45 -0500

    initial import

---
+++ app/components/FKApp.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+
+  var FKApp = React.createClass({
+
+  getInitialState: () => {
+    return {}
+  },
+
+  render: () => {
+    return (
+      <div>
+        FKApp
+      </div>
+    )
+  }
+
+});
+
+module.exports = FKApp;
