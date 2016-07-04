5603cf38326d2efcf4274ebf10dc9d6c2f4b2aba
John Doerfler <johndoerfler@gmail.com>
2016-07-04 16:38:30 -0500

    added scales and tunings drop downs

--- app/components/FretboardMenu.jsx
+++ app/components/FretboardMenu.jsx
@@ -2,11 +2,21 @@ var React = require('react');
 
 var FretboardMenu = React.createClass({
 
-   render: function() {
+  render: function() {
+
     return (
-      <div>
-        <div className="row">
-          <div className="column small-centered large-8 medium-8 small-10 gray">FretboardMenu</div>
+      <div className="row">
+        <div className="column small-centered large-8 medium-8 small-10 gray">
+          <h4>Choose Scale</h4>
+          <select name="scales">
+            <option value="0">Major</option>
+            <option value="1">Minor</option>
+          </select>
+          <h4>Choose Tuning</h4>
+          <select name="tunings">
+            <option value="0">Major</option>
+            <option value="1">Minor</option>
+          </select>
         </div>
       </div>
     )
