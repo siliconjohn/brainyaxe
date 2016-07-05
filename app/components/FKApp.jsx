32901adcb161fb92b559e11ec28aacdbffa76d30
John Doerfler <johndoerfler@gmail.com>
2016-07-04 19:42:59 -0500

    added initial state for tunings and scales and updated FretboardMenu to display the state

--- app/components/FKApp.jsx
+++ app/components/FKApp.jsx
@@ -5,16 +5,26 @@ var FretboardMenu = require('FretboardMenu');
 var FKApp = React.createClass({
 
   getInitialState: function() {
-    return {}
+    return {
+      scales: [
+        { name: 'Major', degrees: '1,2,3,4,5,6,7', key:"0"},
+        { name: 'Blues', degrees: '1,♭3,4,♭5,5,♭7', key:"1"}
+      ],
+      tunings: [
+        { name: 'Standard', notes:'E A D G B E', midiNotes:'40,45,50,55,59,64', key:"0"}
+      ]
+    }
   },
 
   render: function() {
+    var {scales, tunings} = this.state;
+
     return (
       <div>
         <br/>
-        <FretboardMenu/>
+        <FretboardMenu scales={scales} tunings={tunings}/>
         <br/>
-        <Fretboard/>
+        <Fretboard scales={scales}/>
       </div>
     )
   }
--- app/components/FretboardMenu.jsx
+++ app/components/FretboardMenu.jsx
@@ -3,19 +3,34 @@ var React = require('react');
 var FretboardMenu = React.createClass({
 
   render: function() {
+    var {scales,tunings} = this.props;
+
+    var renderScales = () => {
+      return scales.map((scale) => {
+        return (
+          <option key={scale.key}>{scale.name}</option>
+        )
+      })
+    };
+
+    var renderTunings = () => {
+      return tunings.map((tuning) => {
+        return (
+          <option key={tuning.key}>{tuning.name} - {tuning.notes}</option>
+        )
+      })
+    };
 
     return (
       <div className="row">
         <div className="column small-centered large-8 medium-8 small-10 gray">
           <h4>Choose Scale</h4>
           <select name="scales">
-            <option value="0">Major</option>
-            <option value="1">Minor</option>
+            {renderScales()}
           </select>
           <h4>Choose Tuning</h4>
           <select name="tunings">
-            <option value="0">Major</option>
-            <option value="1">Minor</option>
+            {renderTunings()}
           </select>
         </div>
       </div>
