fc0cf9d2976527008f23a355f350e2b32e2bb311
John Doerfler <johndoerfler@gmail.com>
2016-07-06 15:00:13 -0500

    added header

--- app/components/Fretboard.jsx
+++ app/components/Fretboard.jsx
@@ -16,18 +16,49 @@ var Fretboard = React.createClass({
       }).reverse(); // reverse returned array so lowest note is at bottom
     };
 
+    var renderTableHeader = () => {
+      var fretCount = new Array(12); //number of frets
+
+      // TO do: use this to set the width of each fret
+      for(var i=0; i < fretCount.length; i++){
+        fretCount[i] = i+1;
+      }
+
+      var result = [];
+
+      // note header
+      result.push((<th className="header-note-name" key="note">Note</th>))
+
+      // fret numbers
+      result.push(fretCount.map((index) => {
+        return (
+          <th className="header-fret-number" key={index}>{index}</th>
+        )
+      }));
+
+      return result;
+    };
+
     return (
       <div>
         <div className="row">
-          <div className="column small-centered large-10 medium-12 small-12 gray">{tuning.name} - {tuning.notes}</div>
+          <div className="column small-centered large-10 medium-12 small-12">{tuning.name} - {tuning.notes}</div>
         </div>
         <br/>
         <div className="row">
-          <div className="column small-centered large-10 medium-12 small-12 gray">
-            {renderStrings()}
+          <div className="column small-centered large-10 medium-12 small-12">
+            <table className="fretboard-table">
+              <thead>
+                <tr>
+                  {renderTableHeader()}
+                </tr>
+              </thead>
+              <tbody>
+                {renderStrings()}
+              </tbody>
+            </table>
           </div>
         </div>
-
       </div>
     )
   }
