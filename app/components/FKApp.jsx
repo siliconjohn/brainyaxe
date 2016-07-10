a6f53385a2d32d5c791a9136b1dbde27fb50a9b8
John Doerfler <johndoerfler@gmail.com>
2016-07-09 20:59:30 -0500

    added new scales

--- app/components/FKApp.jsx
+++ app/components/FKApp.jsx
@@ -8,9 +8,19 @@ var FKApp = React.createClass({
   getInitialState: function() {
     return {
       scales: [
-        { name: 'No Scale', degrees: '', intervals:[], key:"033z"},
-        { name: 'Major', degrees: '1,2,3,4,5,6,7', intervals:[2,4,5,7,9,11], key:"0zz"},
-        { name: 'Blues', degrees: '1,♭3,4,5,♭7', intervals:[3,5,7,10], key:"1z"}
+        { name: 'No Scale', degrees: '', intervals:[], key:"scale0"},
+        { name: 'Major', degrees: '1,2,3,4,5,6,7', intervals:[2,4,5,7,9,11], key:"scale1"},
+        { name: 'Blues', degrees: '1,♭3,4,5,♭7', intervals:[3,5,7,10], key:"scale2"},
+        { name: 'Natural Minor', degrees: '1,2,♭3,4,5,♭6,♭7', intervals:[2,3,5,7,8,10], key:"scale3"},
+        { name: 'Harmonic Minor', degrees: '1,2,♭3,4,5,♭6,7', intervals:[2,3,5,7,8,11], key:"scale4"},
+        { name: 'Melodic Minor', degrees: '1,2,♭3,4,5,6,7', intervals:[2,3,5,7,9,11], key:"scale5"},
+        { name: 'Ionian (Major)', degrees: '1,2,3,4,5,6,7', intervals:[2,4,5,7,9,11], key:"scale6"},
+        { name: 'Dorian', degrees: '1,2,♭3,4,5,6,♭7', intervals:[2,3,5,7,9,10], key:"scale7"},
+        { name: 'Phrygian', degrees: '1,♭2,♭3,4,5,♭6,♭7', intervals:[1,3,5,7,8,10], key:"scale8"},
+        { name: 'Lydian', degrees: '1,2,3,♯4,5,6,7', intervals:[2,4,6,7,9,11], key:"scale9"},
+        { name: 'Mixolydian', degrees: '1,2,3,4,5,6,♭7', intervals:[2,4,5,7,9,10], key:"scale10"},
+        { name: 'Aeolian (Natural Minor)', degrees: '1,2,♭3,4,5,♭6,♭7', intervals:[2,3,5,7,8,10], key:"scale11"},
+        { name: 'Locrian', degrees: '1,♭2,♭3,4,♭5,♭6,♭7', intervals:[1,3,5,6,8,10], key:"scale12"}
       ],
       tunings: [
         { name: 'Standard', notes:'E,A,D,G,B,E', midiNotes:[40,45,50,55,59,64], key:"d"},
@@ -42,7 +52,7 @@ var FKApp = React.createClass({
         { name: 'Dominant 13th', degrees: 'R,3,5,♭7,9,11,13', intervals:[4,7,10,14,17,21], key:"chord22"}
       ],
       selectedTuningKey: "d",
-      selectedScaleKey: "033z",
+      selectedScaleKey: "scale0",
       selectedScaleNote: "E",
       selectedChordKey: "chord0",
       selectedChordNote: "D",
