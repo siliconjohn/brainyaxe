47556cfca1753e89e004ff7112a4def55b514352
John Doerfler <johndoerfler@gmail.com>
2016-07-18 20:02:37 -0500

    refractored

--- app/components/FretboardFret.jsx
+++ app/components/FretboardFret.jsx
@@ -1,11 +1,9 @@
 var React = require('react')
 
 var FretboardFret = (props) => {
-  var { x, height, width, key} = props
+  var {x, height, width, key} = props
 
-  return (
-    <rect x={x} y="0" width={width} height={height} key={"fret-" + key} className="fret"/>
-  )
+  return (<rect x={x} y="0" width={width} height={height} key={"fret-" + key} className="fret"/>)
 }
 
 module.exports = FretboardFret
