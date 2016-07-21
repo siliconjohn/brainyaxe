7503cf291b0d39c5e48c637d64e22321908acf13
John Doerfler <johndoerfler@gmail.com>
2016-07-21 18:17:48 -0500

    refractored

--- app/components/FretboardOpenString.jsx
+++ app/components/FretboardOpenString.jsx
@@ -2,10 +2,11 @@ var React = require('react')
 
 var FretboardOpenString = (props) => {
   var { y, width, key, note} = props
-  var fontSize = 18
+  var textYOffset = 6
+  var textXOffset = 6
 
   return (
-    <text x={width/2} y={y + (fontSize/2) -3} className="open-string-text" key={key}>{note}</text>
+    <text x={width / 2 - textXOffset} y={ y + textYOffset} className="open-string-text" key={key}>{note}</text>
   )
 }
 
