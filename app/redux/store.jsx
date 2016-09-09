38538553d2ac4599a1a3f1c5b263d19af7d03a88
John Doerfler <johndoerfler@gmail.com>
2016-09-09 18:05:39 -0500

    added sample store.dispatches

--- app/redux/store.jsx
+++ app/redux/store.jsx
@@ -12,6 +12,9 @@ var store = redux.createStore(reducers.appReducers, initialState, redux.compose(
 store.dispatch(actions.changeTuning("basstuning1"))
 
 store.dispatch(actions.changeScale("scale7"))
+store.dispatch(actions.changeChord("chord2"))
+store.dispatch(actions.changeScaleNote("C"))
+store.dispatch(actions.changeChordNote("D"))
 
 module.exports.gg = function() {
   return store.getState()
