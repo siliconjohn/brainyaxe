92bf3ed8b94001f8c0914c00f9513cc5cf9f8b9c
John Doerfler <johndoerfler@gmail.com>
2016-09-13 12:40:10 -0500

    removed no longer needed stuff

--- app/components/FKApp.jsx
+++ app/components/FKApp.jsx
@@ -1,42 +1,15 @@
 var React = require('react');
 import FretboardSVG from 'FretboardSVG'
 import FretboardMenu from 'FretboardMenu';
-var {getObjectForKey, getNotesForArray} = require('../utils');
-
-var { initialState } = require('../initialState')
 
 export var FKApp = React.createClass({
 
-  getInitialState: function() {
-    return initialState;
-  },
-
-  componentWillMount: function() {
-    // set the inital state for things that are calculated by the app
-    var {scales, selectedScaleKey, chords, selectedChordKey, selectedScaleNote,
-        selectedChordNote} = this.state;
-
-    var scale = getObjectForKey(scales, selectedScaleKey);
-    var chord = getObjectForKey(chords, selectedChordKey);
-
-    this.setState({
-      selectedNotesForScale : getNotesForArray(scale, selectedScaleNote),
-      selectedNotesForChord : getNotesForArray(chord, selectedChordNote)
-    })
-  },
-
   render: function() {
-    var {scales, tunings, selectedTuningKey, selectedScaleKey,
-        chords, selectedChordKey, selectedNotesForScale, selectedNotesForChord,
-        selectedChordNote, selectedScaleNote} = this.state;
 
     return (
       <div>
         <br/>
-        <FretboardMenu scales={scales} tunings={tunings} chords={chords} selectedChordNote={selectedChordNote}
-          selectedTuningKey={selectedTuningKey} selectedScaleKey={selectedScaleKey} selectedScaleNote={selectedScaleNote}
-          selectedChordKey={selectedChordKey} 
-          selectedNotesForScale={selectedNotesForScale} selectedNotesForChord={selectedNotesForChord}/>
+        <FretboardMenu/>
         <br/>
         <FretboardSVG/>
       </div>
