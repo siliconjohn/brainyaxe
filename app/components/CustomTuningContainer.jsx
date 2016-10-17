46e821bfa62dfc136f7cf208677a35b3c1cb6ac9
John Doerfler <johndoerfler@gmail.com>
2016-10-17 13:47:37 +0700

    changed to use numberOfCustomTuningStrings to render <CustomTuningChooser/>s

--- app/components/CustomTuningContainer.jsx
+++ app/components/CustomTuningContainer.jsx
@@ -1,23 +1,30 @@
-const React = require('react')
+var React = require('react')
+var { connect } = require('react-redux')
+var actions = require('../redux/actions')
 import CustomTuningChooser from 'CustomTuningChooser'
 
 export var CustomTuningContainer = React.createClass({
 
   render: function () {
+    var num = this.props.numberOfCustomTuningStrings
+
     return (
       <div>
         <p>Or Create a Custom Tuning</p>
-        <CustomTuningChooser key="0" stringNumber="0"/>
-        <CustomTuningChooser key="1" stringNumber="1"/>
-        <CustomTuningChooser key="2" stringNumber="2"/>
-        <CustomTuningChooser key="3" stringNumber="3"/>
-        <CustomTuningChooser key="4" stringNumber="4"/>
-        <CustomTuningChooser key="5" stringNumber="5"/>
-        <CustomTuningChooser key="6" stringNumber="6"/>
-        <CustomTuningChooser key="7" stringNumber="7"/>
+        {
+          Array( num ).fill().map(( _, i ) => {
+            return (
+              <CustomTuningChooser key={ i } stringNumber={ i }/>
+            )
+          })
+        }
       </div>
     )
   }
 })
 
-module.exports = CustomTuningContainer
+export default connect(( state ) => {
+  return {
+      numberOfCustomTuningStrings: state.numberOfCustomTuningStrings
+  }
+})(CustomTuningContainer)
