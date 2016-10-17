ffcd6be639c9c3a6867d1c356ede286406832c33
John Doerfler <johndoerfler@gmail.com>
2016-10-17 13:47:51 +0700

    refactored, just added some spaces

--- app/components/customtunings/CustomTuningChooser.jsx
+++ app/components/customtunings/CustomTuningChooser.jsx
@@ -9,29 +9,31 @@ export var CustomTuningChooser = React.createClass({
   render: function() {
     var { dispatch, stringNumber, tunings } = this.props
     var customTuning = utils.getObjectForKey( tunings, "custom")
-    var selectedMidiNote = customTuning.midiNotes[stringNumber].toString()
+    var selectedMidiNote = customTuning.midiNotes[ stringNumber ].toString()
 
     return (
       <select value={ selectedMidiNote } className="custom-tuning-select"
         onChange={ (e) => {
-          dispatch( actions.changeCustomTuningNote( stringNumber, parseInt(e.target.value)))
+          dispatch( actions.changeCustomTuningNote( stringNumber, parseInt( e.target.value )))
           dispatch( actions.changeTuning( "custom" ))
         }}>
 
-        { utils.notesNameTable.map(( note, index ) => {
-          var tempProps =  {note:note, index:index}
+        {
+          utils.notesNameTable.map(( note, index ) => {
+            var tempProps = { note:note, index:index }
 
-          return (
-            <CustomTuningOption key={ index } {...tempProps}/>
-          )
-        })}
+            return (
+              <CustomTuningOption key={ index } { ...tempProps }/>
+            )
+          })
+        }
 
       </select>
     )
   }
 });
 
-export default connect((state) => {
+export default connect(( state ) => {
   return {
     tunings: state.tunings
   }
