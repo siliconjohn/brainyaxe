17f8ed71ada2a22b03bf279f8517e2e56b0a3710
John Doerfler <johndoerfler@gmail.com>
2016-10-18 08:22:10 +0700

    removed commented code

--- app/app.jsx
+++ app/app.jsx
@@ -2,13 +2,12 @@ var React = require('react')
 var ReactDOM = require('react-dom')
 var { Provider } = require('react-redux')
 var store = require('./redux/store')
-
-//var AppContainer = require('AppContainer');
+ 
 import AppContainer from 'AppContainer';
 require('style!css!sass!applicationStyles');
 
 var newStore = store.createStore();
- 
+
 ReactDOM.render(
   <Provider store={newStore}>
     <AppContainer/>
