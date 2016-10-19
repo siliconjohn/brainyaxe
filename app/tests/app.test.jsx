2ce5d7541279189c22734e5448e98e334f2f11ce
John Doerfler <johndoerfler@gmail.com>
2016-10-19 19:07:05 +0700

    not needed

--- app/tests/app.test.jsx
+++ app/tests/app.test.jsx
@@ -3,16 +3,10 @@ var expect = require('expect');
 var ReactDOM = require('react-dom');
 var TestUtils = require('react-addons-test-utils');
 
-var AppContainer = require('AppContainer');
-
 describe('App', () => {
 
   it('should exist', () => {
     expect(1).toBe(1);
   });
 
-  it("loads without error", function () {
-    var AppContainer = TestUtils.renderIntoDocument(<AppContainer/>);
-    expect(AppContainer).toExist();
-  });
 });
