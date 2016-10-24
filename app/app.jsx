d1d3b0013ef20c2853f16d6b2db864a13453df87
John Doerfler <johndoerfler@gmail.com>
2016-10-24 15:36:21 +0700

    changed initialState to be passes to store.createStore() to facilitate testing

--- app/app.jsx
+++ app/app.jsx
@@ -2,11 +2,12 @@ var React = require('react')
 var ReactDOM = require('react-dom')
 var { Provider } = require('react-redux')
 var store = require('./redux/store')
- 
+var { initialState } = require('./initialState')
+
 import AppContainer from 'AppContainer';
 require('style!css!sass!applicationStyles');
 
-var newStore = store.createStore();
+var newStore = store.createStore( initialState );
 
 ReactDOM.render(
   <Provider store={newStore}>
--- app/redux/store.jsx
+++ app/redux/store.jsx
@@ -1,9 +1,8 @@
 var redux = require('redux')
 var actions = require('./actions')
 var reducers = require('./reducers')
-var { initialState } = require('../initialState')
 
-export var createStore = () => {
+export var createStore = ( initialState ) => {
 
   var store = redux.createStore(reducers.appReducers, initialState, redux.compose(
     window.devToolsExtension ? window.devToolsExtension() : (f) => f
