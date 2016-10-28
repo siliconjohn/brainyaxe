de0af2c31f7905f280f952b372fd31c55a971e83
John Doerfler <johndoerfler@gmail.com>
2016-10-28 11:50:29 +0700

    added 3 toolbar buttons

--- app/components/Toolbar.jsx
+++ app/components/Toolbar.jsx
@@ -7,10 +7,17 @@ var Toolbar = React.createClass({
     return (
       <div className="title-bar">
         <div className="title-bar-left">
-          <h5>Fretboard Kit</h5>
+          <h5>Fret Kit</h5>
         </div>
         <div className="title-bar-right">
           <button className="menu-icon" type="button"></button>
+        </div>
+        <div className="row column medium-6 medium-centered small-8 small-centered">
+          <ul className="tabs text-center shadow toolbar-tabs" data-tabs id="toolbar-tabs">
+            <li className="tabs-title"><a href="#scales-panel">Scales</a></li>
+            <li className="tabs-title is-active"><a href="#tunings-panel" aria-selected="true">Tunings</a></li>
+            <li className="tabs-title"><a href="#chords-panel">Chords</a></li>
+          </ul>
         </div>
       </div>
     )
