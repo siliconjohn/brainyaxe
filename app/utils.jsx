f1a65428d48aa2841a84c99bd654b0368104c1c8
John Doerfler <johndoerfler@gmail.com>
2016-11-16 18:58:46 +0700

    added comment

--- app/utils.jsx
+++ app/utils.jsx
@@ -1,3 +1,4 @@
+// all possible notes, the array index is the midi number
 export const noteNamesTable = ["C","C♯/D♭","D","D♯/E♭","E","F","F♯/G♭","G","G♯/A♭","A","A♯/B♭","B","C",
                         "C♯/D♭","D","D♯/E♭","E","F","F♯/G♭","G","G♯/A♭","A","A♯/B♭","B","C",
                         "C♯/D♭","D","D♯/E♭","E","F","F♯/G♭","G","G♯/A♭","A","A♯/B♭","B","C",
