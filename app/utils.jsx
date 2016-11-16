da5ab57f4fb36b7ab80252cdbe8f4848672010db
John Doerfler <johndoerfler@gmail.com>
2016-11-16 17:13:51 +0700

    removed commented code

--- app/utils.jsx
+++ app/utils.jsx
@@ -38,7 +38,7 @@ module.exports.isNoteInArray = function( midiNumber, noteArray ) {
     return midiNote === noteName  ? true : false;
   })
 }
- 
+
 module.exports.getNotesForArray = function( scale, startNoteName ) {
   if(scale.intervals.length == 0) return [];
 
@@ -83,28 +83,3 @@ module.exports.playMidiNote = function ( note ) {
     console.log("Timbre not loaded")
   }
 }
-
-// This was used to generate the notes table, saving incase it
-// needs to be changed
-// var scale = new Array(11);
-//
-// scale[0] = 'C';
-// scale[1] = 'C♯/D♭';
-// scale[2] = 'D';
-// scale[3] = 'D♯/E♭';
-// scale[4] = 'E';
-// scale[5] = 'F';
-// scale[6] = 'F♯/G♭';
-// scale[7] = 'G';
-// scale[8] = 'G♯/A♭';
-// scale[9] = 'A';
-// scale[10] = 'A♯/B♭';
-// scale[11] = 'B';
-//
-// table = [];
-//
-// for(var i=0; i< 11; i++) {
-//   table= table.concat(scale);
-// }
-//
-// console.log(table.toString());
