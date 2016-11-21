48c5130057b1da99df262296d141a1f8610f8ba8
John Doerfler <johndoerfler@gmail.com>
2016-11-21 16:06:18 +0700

    removed foundation js, not needed anymore

--- karma.conf.js
+++ karma.conf.js
@@ -9,10 +9,10 @@ module.exports = function (config) {
       'node_modules/jquery/dist/jquery.min.js',
       //'node_modules/foundation-sites/dist/foundation.min.js',
       // only load the js I need from foundation
-      'node_modules/foundation-sites/js/foundation.core.js',
-      'node_modules/foundation-sites/js/foundation.util.mediaQuery.js',
-      'node_modules/foundation-sites/js/foundation.tabs.js',
-      'node_modules/foundation-sites/js/foundation.util.keyboard.js',
+      // 'node_modules/foundation-sites/js/foundation.core.js',
+      // 'node_modules/foundation-sites/js/foundation.util.mediaQuery.js',
+      // 'node_modules/foundation-sites/js/foundation.tabs.js',
+      // 'node_modules/foundation-sites/js/foundation.util.keyboard.js',
       'app/tests/**/*.test.jsx'
     ],
     preprocessors: {
