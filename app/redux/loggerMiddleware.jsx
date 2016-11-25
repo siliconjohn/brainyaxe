641d01449973976a29e055f7a3162f64e597becd
John Doerfler <johndoerfler@gmail.com>
2016-11-25 15:45:41 +0700

    added redux middleware to track all events with GA

---
+++ app/redux/loggerMiddleware.jsx
@@ -0,0 +1,22 @@
+import ReactGA from 'react-ga'
+
+module.exports.logger = ({ getState }) => {
+
+  return ( next ) => ( action ) => {
+
+    // clone the action and delete the type, it's redundant
+    var newAction = Object.assign( {}, action )
+    delete newAction.type
+
+  //  console.log("Changed: " + JSON.stringify( newAction ));
+    ReactGA.event({
+      category: action.type,
+      action: "Changed: " + JSON.stringify( newAction )
+    })
+
+    // call the next dispatch method in the middleware chain.
+    let returnValue = next( action )
+
+    return returnValue
+  }
+}
--- app/redux/store.jsx
+++ app/redux/store.jsx
@@ -1,12 +1,14 @@
 var redux = require('redux')
 var actions = require('actions')
 var reducers = require('reducers')
+var { logger } = require('loggerMiddleware')
 
 export var createStore = ( initialState ) => {
 
-  var store = redux.createStore(reducers.appReducers, initialState, redux.compose(
-    window.devToolsExtension ? window.devToolsExtension() : (f) => f
-  ))
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || redux.compose
+
+  var store = redux.createStore( reducers.appReducers, initialState, composeEnhancers(
+                                 redux.applyMiddleware( logger )))
 
   return store
 }
