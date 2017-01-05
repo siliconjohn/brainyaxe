fd9cffc32f321d6895d31146a84d82d5fcf553a2
John Doerfler <johndoerfler@gmail.com>
2017-01-05 17:25:40 +0700

    changed to inline style

--- FretboardIntervalChooser.jsx
+++ FretboardIntervalChooser.jsx
@@ -3,12 +3,19 @@ import { connect } from 'react-redux'
 
 const intervals = [ 1, 2, 3, 4, 5, 6, 7 ]
 
+// inline style because I think foundation.js is messing with it and this
+// is the only way I can get it to work
+const linkStyle = {
+  paddingTop: "4px",
+  paddingBottom: "4px"
+}
+
 export class FretboardIntervalChooser extends React.Component {
 
   render() {
     return (
       <ul className="dropdown menu" data-dropdown-menu>
-        <li className="dropdowntitle"><a href="#" className="dropdowntitle">Highlight</a>
+        <li ><a href="#" style={ linkStyle }>Highlight</a>
         <ul className="menu dark">
           <li><a href="#">Item 1</a></li>
           <li><a href="#">Item 2</a></li>
